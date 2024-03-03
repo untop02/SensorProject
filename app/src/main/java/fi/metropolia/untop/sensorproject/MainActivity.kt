a57a362fe3df1e2c24ded6b729bdd7c1116d73bc
Glatus <janipudas1@gmail.com>
2024-03-03 19:14:34 +0200

    Rikoin ja korjasin kai 🙃

--- app/src/main/java/fi/metropolia/untop/sensorproject/MainActivity.kt
+++ app/src/main/java/fi/metropolia/untop/sensorproject/MainActivity.kt
@@ -1,14 +1,17 @@
 package fi.metropolia.untop.sensorproject
 
 import android.Manifest
-import android.os.Build
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
@@ -30,15 +33,26 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import fi.metropolia.untop.sensorproject.ui.theme.SensorProjectTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), SensorEventListener {
     private val viewModel: MyViewModel by viewModels()
-    @RequiresApi(Build.VERSION_CODES.Q)
+    private lateinit var mSensorManager: SensorManager
+    private var mTemp: Sensor? = null
+    private var mLight: Sensor? = null
+    private var mPressure: Sensor? = null
+    private var mHumidity: Sensor? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel.makeTestData(viewModel.ambientTemp)
-        viewModel.makeTestData(viewModel.humidity)
-        viewModel.makeTestData(viewModel.light)
-        viewModel.makeTestData(viewModel.pressure)
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mTemp = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+        mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        mPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
+        mHumidity = mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY)
+        /*
+                viewModel.makeTestData(viewModel.ambientTemp)
+                viewModel.makeTestData(viewModel.humidity)
+                viewModel.makeTestData(viewModel.light)
+                viewModel.makeTestData(viewModel.pressure)
+        */
         val requiredPermissions: Array<String> = arrayOf(
             Manifest.permission.BLUETOOTH,
             Manifest.permission.BLUETOOTH_ADMIN,
@@ -115,14 +129,44 @@ class MainActivity : ComponentActivity() {
                             composable(Destinations.Settings.route) {
                                 Settings(modifier = Modifier)
                             }
-                            composable(Destinations.Graph.route.plus("?observedName={observedName}"), arguments = listOf(
-                                navArgument("observedName") {defaultValue = "null"})) {
-                                Graph(modifier = Modifier, viewModel, it.arguments?.getString("observedName"))
+                            composable(
+                                Destinations.Graph.route.plus("?observedName={observedName}"),
+                                arguments = listOf(
+                                    navArgument("observedName") { defaultValue = "null" })
+                            ) {
+                                Graph(
+                                    modifier = Modifier,
+                                    viewModel,
+                                    it.arguments?.getString("observedName")
+                                )
                             }
                         }
                     }
                 }
             }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mSensorManager.registerListener(this, mTemp, SensorManager.SENSOR_DELAY_NORMAL)
+        mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL)
+        mSensorManager.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL)
+        mSensorManager.registerListener(this, mHumidity, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mSensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    override fun onSensorChanged(event: SensorEvent) {
+        when (event.sensor.type) {
+            Sensor.TYPE_AMBIENT_TEMPERATURE -> viewModel.ambientTemp.postValue(event.values[0].toDouble())
+            Sensor.TYPE_LIGHT -> viewModel.light.postValue(event.values[0].toDouble())
+            Sensor.TYPE_PRESSURE -> viewModel.pressure.postValue(event.values[0].toDouble())
+            Sensor.TYPE_RELATIVE_HUMIDITY -> viewModel.humidity.postValue(event.values[0].toDouble())
         }
     }
 }
